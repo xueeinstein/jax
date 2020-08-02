d9d7e979e52aa3390da1ca9f703eb6b02cd6b761
Bill Xue <xueeinstein@gmail.com>
2020-08-02 23:09:58 +0800

    generate compile_commands.json

--- build_jaxlib.sh
+++ build_jaxlib.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 export GCC_HOST_COMPILER_PATH=/usr/bin/gcc-9
+
+# generate compile_commands.json
+TF_CUDA_PATHS=/usr,/home/extend/cuda /home/billxue/Code/tool/bazel-compilation-database/generate.sh
+
 TF_CUDA_PATHS=/usr,/home/extend/cuda python build/build.py \
   --enable_march_native \
   --enable_mkl_dnn \
